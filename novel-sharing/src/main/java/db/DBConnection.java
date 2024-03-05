51f23b2d096b3773e4be21b84bf61f4600f62f6c
TaigaShiokawa1122 <taiga.shiokawa@icloud.com>
2024-03-05 19:34:53 +0900

    test commit

---
+++ novel-sharing/src/main/java/db/DBConnection.java
@@ -0,0 +1,20 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		final String URL = "jdbc:mysql://localhost/kobebook?useUnicode=true&characterEncoding=utf8";
+		final String USER = "root";
+		final String PASS = "root";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(URL,USER,PASS);
+		return con;
+		
+	}
+
+}
